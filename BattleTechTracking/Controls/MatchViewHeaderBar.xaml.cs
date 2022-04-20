ceb817eb610207225b9bb7242dbc60fdfc1f538f
Christopher Nye <icedcrow@yahoo.com>
2022-04-20 09:40:05 -0700

    MatchView header bar turned into its own control.

---
+++ BattleTechTracking/Controls/MatchViewHeaderBar.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BattleTechTracking.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MatchViewHeaderBar : ContentView
+    {
+        public static readonly BindableProperty CloseCommandProperty = BindableProperty.Create(
+            nameof(CloseCommand),
+            typeof(ICommand),
+            typeof(MatchViewHeaderBar));
+
+        public static readonly BindableProperty OkCommandProperty = BindableProperty.Create(
+            nameof(OkCommand),
+            typeof(ICommand),
+            typeof(MatchViewHeaderBar));
+
+        public static readonly BindableProperty SettingsCommandProperty = BindableProperty.Create(
+            nameof(SettingsCommand),
+            typeof(ICommand),
+            typeof(MatchViewHeaderBar));
+
+        public static readonly BindableProperty NewRoundCommandProperty = BindableProperty.Create(
+            nameof(NewRoundCommand),
+            typeof(ICommand),
+            typeof(MatchViewHeaderBar));
+
+        public static readonly BindableProperty ReportsCommandProperty = BindableProperty.Create(
+            nameof(ReportsCommand),
+            typeof(ICommand),
+            typeof(MatchViewHeaderBar));
+
+        public Command CloseCommand
+        {
+            get => (Command)GetValue(MatchViewHeaderBar.CloseCommandProperty);
+            set => SetValue(MatchViewHeaderBar.CloseCommandProperty, value);
+        }
+
+        public Command OkCommand
+        {
+            get => (Command)GetValue(MatchViewHeaderBar.OkCommandProperty);
+            set => SetValue(MatchViewHeaderBar.OkCommandProperty, value);
+        }
+
+        public Command NewRoundCommand
+        {
+            get => (Command)GetValue(MatchViewHeaderBar.NewRoundCommandProperty);
+            set => SetValue(MatchViewHeaderBar.NewRoundCommandProperty, value);
+        }
+
+        public Command SettingsCommand
+        {
+            get => (Command)GetValue(MatchViewHeaderBar.SettingsCommandProperty);
+            set => SetValue(MatchViewHeaderBar.SettingsCommandProperty, value);
+        }
+
+        public Command ReportsCommand
+        {
+            get => (Command)GetValue(MatchViewHeaderBar.ReportsCommandProperty);
+            set => SetValue(MatchViewHeaderBar.ReportsCommandProperty, value);
+        }
+
+        public MatchViewHeaderBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
