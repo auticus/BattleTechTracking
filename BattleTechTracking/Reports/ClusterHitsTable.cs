036f2f25f15f60366037cbb09d1c8c5f9e509de8
Christopher Nye <icedcrow@yahoo.com>
2022-05-11 08:42:37 -0700

    Cluster Hits Table

---
+++ BattleTechTracking/Reports/ClusterHitsTable.cs
@@ -0,0 +1,60 @@
+﻿using Xamarin.Forms;
+
+namespace BattleTechTracking.Reports
+{
+    internal class ClusterHitsTable : BaseChart
+    {
+        private const int FULL_COL_SPAN = 8;
+
+        public ClusterHitsTable()
+        {
+            LoadEntries();
+        }
+
+        public override Grid GenerateChart()
+        {
+            const int startingRow = 2;
+            var grid = GenerateGridAndRowColumnDefinitions(DefineChart());
+            InjectChartHeader(grid, "CLUSTER HITS TABLE", FULL_COL_SPAN, Color.Gold);
+            InjectColumnTextDefinitions(grid);
+            InjectBody(grid, startingRow);
+            return grid;
+        }
+
+        private void LoadEntries()
+        {
+            ChartEntries.Add(new[] { "2", "1", "1", "1", "2", "3", "5", "6" });
+            ChartEntries.Add(new[] { "3", "1", "2", "2", "2", "3", "5", "6" });
+            ChartEntries.Add(new[] { "4", "1", "2", "2", "3", "4", "6", "9" });
+            ChartEntries.Add(new[] { "5", "1", "2", "3", "3", "6", "9", "12" });
+            ChartEntries.Add(new[] { "6", "1", "2", "3", "4", "6", "9", "12" });
+            ChartEntries.Add(new[] { "7", "1", "3", "3", "4", "6", "9", "12" });
+            ChartEntries.Add(new[] { "8", "2", "3", "3", "4", "6", "9", "12" });
+            ChartEntries.Add(new[] { "9", "2", "3", "4", "5", "8", "12", "16" });
+            ChartEntries.Add(new[] { "10", "2", "3", "4", "5", "8", "12", "16" });
+            ChartEntries.Add(new[] { "11", "2", "4", "5", "6", "10", "15", "20" });
+            ChartEntries.Add(new[] { "12", "2", "4", "5", "6", "10", "15", "20" });
+        }
+
+        private ChartDefinition DefineChart()
+        {
+            var rows = new[] { 32, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25 };
+            var cols = new[] { 100, 50, 50, 50, 50, 50, 50, 50 };
+            return new ChartDefinition(rows, cols);
+        }
+
+        private void InjectColumnTextDefinitions(Grid grid)
+        {
+            const int row = 1;
+            var col = 0;
+            InjectLabelInColumn(grid, "Roll (2d6)", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "2", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "4", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "5", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "6", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "10", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "15", row, col++, Color.Gold);
+            InjectLabelInColumn(grid, "20", row, col, Color.Gold);
+        }
+    }
+}
--- BattleTechTracking/Reports/DataReport.cs
+++ BattleTechTracking/Reports/DataReport.cs
@@ -4,6 +4,7 @@ namespace BattleTechTracking.Reports
 {
     public class DataReport
     {
+        public const string CLUSTER_HITS_TABLE = "Cluster Hits Table";
         public const string DAMAGE_REPORT = "Damage Report";
         public const string DETERMINING_CRITICAL_HITS = "Determining Critical Hits Table";
         public const string KICK_LOCATION_TABLE = "Kick Location Table";
@@ -14,6 +15,7 @@ namespace BattleTechTracking.Reports
         {
             var reports = new List<string>
             {
+                CLUSTER_HITS_TABLE,
                 DAMAGE_REPORT,
                 DETERMINING_CRITICAL_HITS,
                 KICK_LOCATION_TABLE,
@@ -32,6 +34,7 @@ namespace BattleTechTracking.Reports
         {
             var dictionary = new Dictionary<string,dynamic>
             {
+                { CLUSTER_HITS_TABLE, new ClusterHitsTable()},
                 { DAMAGE_REPORT, new DamageReport() },
                 { DETERMINING_CRITICAL_HITS, new DetermineCritHitsTable()},
                 { KICK_LOCATION_TABLE, new KickLocationTable()},
